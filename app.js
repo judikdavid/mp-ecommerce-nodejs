eadac802c97eb5cf2c8021ba99e0b0c285f896a7
davidjudik <david.judik@instructure.com>
2022-08-15 16:22:32 +0200

    Add implementation

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var port = process.env.PORT || 3000
+const mercadopago = require('mercadopago');
+const access_token = 'APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181'
+const integrator_id = 'dev_24c65fb163bf11ea96500242ac130004'
+
+mercadopago.configure({
+    access_token,
+    integrator_id,
+});
+
+const host = 'https://e6a6-89-132-231-7.eu.ngrok.io'
+const external_reference = 'gonzalo.egonzalez@gmail.com'
 
 var app = express();
- 
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.use(express.static('assets'));
- 
+
 app.use('/assets', express.static(__dirname + '/assets'));
 
 app.get('/', function (req, res) {
@@ -16,7 +27,91 @@ app.get('/', function (req, res) {
 });
 
 app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+    const { title, unit, price, img } = req.query
+    // Create a preference object
+    const id = 1234
+    const unit_price = Number(price)
+    const quantity = Number(unit)
+    const description = 'Dispositio móvil de Tienda e-commerce'
+    const picture_url = `${host}${img}`
+
+    let preference = {
+        items: [
+            {
+                id,
+                title,
+                unit_price,
+                quantity,
+                description,
+                picture_url,
+            }
+        ],
+        "payer": {
+            "name": "Lalo",
+            "surname": "Landa",
+            "email": "test_user_81131286@testuser.com",
+            "phone": {
+                "area_code": "11",
+                "number": 44444444
+            },
+            "identification": {
+                "type": "DNI",
+                "number": "12345678"
+            },
+            "address": {
+                "street_name": "Falsa",
+                "street_number": 123,
+                "zip_code": "5700"
+            }
+        },
+        "payment_methods": {
+            "excluded_payment_methods": [
+                {
+                    // TODO: change to visa
+                    "id": "master"
+                }
+            ],
+            "excluded_payment_types": [
+                {
+                    "id": "ticket"
+                }
+            ],
+            "installments": 6
+        },
+        "back_urls": {
+            "success": `${host}/feedback`,
+            "failure": `${host}/feedback`,
+            "pending": `${host}/feedback`,
+        },
+        auto_return: "approved",
+        "notification_url": `${host}/ipn`,
+        external_reference,
+    };
+
+    mercadopago.preferences.create(preference)
+        .then(function (response) {
+            console.log(response)
+            // This value replaces the String "<%= global.id %>" in your HTML
+            return res.render('detail', { ...req.query, preference: response.body.id, init_point: response.body.init_point })
+        }).catch(function (error) {
+            console.log(error);
+        });
+
+});
+
+app.get('/feedback', function (req, res) {
+    res.json({
+        Payment: req.query.payment_id,
+        Status: req.query.status,
+        MerchantOrder: req.query.merchant_order_id,
+        ExternalReference: req.query.external_reference,
+        SiteId: req.query.site_id,
+        PreferenceId: req.query.preference_id,
+    });
+});
+
+app.get('/ipn', function (req, res) {
+    console.log(req)
 });
 
 app.listen(port);
